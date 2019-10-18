e30e9aec465834f33951646f99abbdb908c26346
wangjing <942252530@qq.com>
2019-10-18 17:31:53 +0800

    学习笔记初始化V1.0

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: 'Wj Blog',
+  description: 'This is a blog example built by VuePress',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: '博文',
+        link: '/',
+      },
+      {
+        text: '学习分类',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/ulivz',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/_ulivz',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+}
